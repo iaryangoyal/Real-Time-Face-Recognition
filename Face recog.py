3f2f81bca0f0b7e821afb8b775da2d21ca2b19bc
Aryan Goyal <73420647+iaryangoyal@users.noreply.github.com>
2021-05-12 16:00:02 +0530

    Add files via upload

---
+++ Face recog.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+face_Reg = cv2.CascadeClassifier('haarcascade_frontalface_default')
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # set Width
+cap.set(4,480) # set Height
+while True:
+     ret, vid = cap.read()
+     faces = face_Reg.detectMultiScale(
+        vid,     
+        scaleFactor=1.2,
+        minNeighbors=5,     
+        minSize=(20, 20)
+    )
+     for (x, y, w, h) in faces:
+        cv2.rectangle(vid, (x, y), (x+w, y+h), (0, 255, 0), 2,cv2.CASCADE_SCALE)
+     cv2.imshow('frame', vid)
+     k = cv2.waitKey(30) & 0xff
+     if k == 27: # press 'ESC' to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
